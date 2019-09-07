e6e9168573071b9b1c5491b180d29051e44625b9
unknown <h.e.ergaster@gmail.com>
2019-09-07 11:46:03 -0700

    photos

--- bamazon.sql
+++ bamazon.sql
@@ -17,13 +17,13 @@ Select * FROM products;
 
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
-VALUES (00101, "jockstap", "soccer", 69.99, 20),
-	   (00212, "jerseys", "basketball", 79.99, 10),
-	   (03013, "helmet", "football", 39.99, 5),
-	   (42000, "birdie", "badminton", 165.99, 14),
-	   (05004, "pants", "football", 70.99, 15),
-	   (06109, "shorts", "soccer", 20.99, 19),
-	   (70200, "gloves", "baseball", 69.99, 11),
-	   (80008, "bats", "baseball", 69.99, 10),
-	   (91003, "ball", "tennis", 9.99, 19),
-	   (10009, "jockstap", "basketball", 89.99, 17)
+VALUES (001, "jockstap", "soccer", 69.99, 20),
+	   (012, "jerseys", "basketball", 79.99, 10),
+	   (033, "helmet", "football", 39.99, 5),
+	   (400, "birdie", "badminton", 165.99, 14),
+	   (004, "pants", "football", 70.99, 15),
+	   (069, "shorts", "soccer", 20.99, 19),
+	   (700, "gloves", "baseball", 69.99, 11),
+	   (808, "bats", "baseball", 69.99, 10),
+	   (003, "ball", "tennis", 9.99, 19),
+	   (109, "jockstap", "basketball", 89.99, 17)
